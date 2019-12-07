7ebe85e8be83cfed6a78a34b64de06874e3c551a
saikatsafiq <saikat25.g@gmail.com>
2019-12-07 10:51:58 +0600

    LoginRegistrationApplication - initial commit. view pages added.

---
+++ src/main/java/com/example/SocialCommunication/web/RegistrationController.java
@@ -0,0 +1,5 @@
+package com.example.SocialCommunication.web;
+
+public class RegistrationController {
+
+}
---
+++ src/test/java/com/example/SocialCommunication/SocialCommunicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SocialCommunication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialCommunicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
